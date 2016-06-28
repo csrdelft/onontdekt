9b330dbbcd7e6544b20600b62e2ae8cf9ee3822e
job <Manduro@users.noreply.github.com>
2016-06-28 02:40:01 +0200

    Travis paths

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Exit with nonzero exit code if anything fails
 set -e
 
 # Get the deploy key
+cd /home/travis/build/Manduro/onontdekt/.travis
 ENCRYPTED_KEY_VAR="encrypted_40378fca9f07_key"
 ENCRYPTED_IV_VAR="encrypted_40378fca9f07_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
@@ -14,6 +15,7 @@ eval `ssh-agent -s`
 ssh-add deploy_key
 
 # Deploy for web app
+cd /home/travis/build
 git config --global user.email "contact@travis-ci.com"
 git config --global user.name "Travis CI"
 git clone --quiet --branch=master git@github.com:csrdelft/lustrum.git release > /dev/null
