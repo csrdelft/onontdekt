6cd747bd2a988f0f14d9e2228e9a0d391794097f
job <Manduro@users.noreply.github.com>
2016-06-28 02:55:10 +0200

    Another go

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,8 +3,12 @@
 # Exit with nonzero exit code if anything fails
 set -e
 
+# Deploy for hybrid app
+cd /home/travis/build/Manduro/onontdekt
+ionic upload --email $IONIC_EMAIL --password $IONIC_PASSWORD --note "Travis deploy $TRAVIS_BUILD_NUMBER" --deploy=staging
+
 # Get the deploy key
-cd /home/travis/build/Manduro/onontdekt/.travis
+cd .travis
 ENCRYPTED_KEY_VAR="encrypted_40378fca9f07_key"
 ENCRYPTED_IV_VAR="encrypted_40378fca9f07_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
@@ -20,10 +24,8 @@ git config --global user.email "contact@travis-ci.com"
 git config --global user.name "Travis CI"
 git clone --quiet --branch=master git@github.com:csrdelft/lustrum.git release > /dev/null
 cd release
-cp -Rf /home/travis/build/Manduro/onontdekt/www/ .
+rm -rf ./*
+cp -Rf /home/travis/build/Manduro/onontdekt/www/ ./
 git add .
 git commit -m "Travis deploy $TRAVIS_BUILD_NUMBER"
 git push --force --quiet git@github.com:csrdelft/lustrum.git master > /dev/null
-
-# Deploy for hybrid app
-ionic upload --email $IONIC_EMAIL --password $IONIC_PASSWORD --note "Travis deploy $TRAVIS_BUILD_NUMBER" --deploy=staging
