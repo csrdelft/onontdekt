4f6eff00be4a96e3c90108fe233974c68b4f05b6
job <Manduro@users.noreply.github.com>
2016-11-15 20:56:10 +0100

    Dev proxy for api to prevent cors issues

--- src/constants/app-settings.ts
+++ src/constants/app-settings.ts
@@ -1,5 +1,7 @@
+import { isDevMode } from '@angular/core';
+
 export class AppSettings {
   static get API_ENDPOINT(): string {
-    return 'https://csrdelft.nl/API/2.0';
+    return isDevMode() ? '/api-proxy' : 'https://csrdelft.nl/API/2.0';
   }
 }
