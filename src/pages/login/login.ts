aabf5be780573d5403096a17034ecafd55e4a60b
Job van Stiphout <job@manduro.nl>
2017-04-10 18:17:58 +0200

    refactor(login): use the form builder

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertController, IonicPage, LoadingController, NavController } from 'ionic-angular';
 
 import { AuthService } from '../../providers/auth';
@@ -9,49 +9,50 @@ import { TabsPage } from '../tabs/tabs';
   segment: 'inloggen'
 })
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'login-page',
   templateUrl: 'login.html'
 })
-export class LoginPage {
-  login: {
-    username?: string;
-    password?: string;
-  } = {};
-  submitted: boolean = false;
+export class LoginPage implements OnInit {
+  loginForm: FormGroup;
 
   constructor(
     private authService: AuthService,
     private navCtrl: NavController,
     private alertCtrl: AlertController,
+    private formBuilder: FormBuilder,
     private loadingCtrl: LoadingController
   ) {}
 
-  onLogin(form: NgForm) {
-    this.submitted = true;
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
-    if (form.valid) {
-      let loading = this.loadingCtrl.create({
-        content: 'Inloggen...'
-      });
+  login() {
+    let loading = this.loadingCtrl.create({
+      content: 'Inloggen...'
+    });
 
-      loading.present();
+    loading.present();
 
-      this.authService
-        .login(this.login.username, this.login.password)
-        .then(() => {
-          this.navCtrl.push(TabsPage).then(() => {
-            loading.dismiss();
-          });
-        })
-        .catch(error => {
+    this.authService
+      .login(this.loginForm.value['username'], this.loginForm.value['password'])
+      .then(() => {
+        this.navCtrl.push(TabsPage).then(() => {
           loading.dismiss();
-          let alert = this.alertCtrl.create({
-            title: 'Inloggen mislukt',
-            message: 'Pff, die verplicht ingewikkelde wachtwoorden ook... Het kan zijn dat je even moet wachten voor je een nieuwe poging mag wagen.',
-            buttons: ['Probeer het nog eens']
-          });
-          alert.present();
         });
-    }
+      })
+      .catch(error => {
+        loading.dismiss();
+        let alert = this.alertCtrl.create({
+          title: 'Inloggen mislukt',
+          message: 'Pff, die verplicht ingewikkelde wachtwoorden ook... Het kan zijn dat je even moet wachten voor je een nieuwe poging mag wagen.',
+          buttons: ['Probeer het nog eens']
+        });
+        alert.present();
+      });
   }
 }
