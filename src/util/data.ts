aab7120f1800f846de36b4c22e7af4879b8d76a7
Job van Stiphout <job@manduro.nl>
2017-05-13 00:24:23 +0200

    fix(util): correct local date time parsing

--- src/util/data.ts
+++ src/util/data.ts
@@ -12,8 +12,13 @@ export function parseJsonDates(res: Response): any {
 export function reviveDateTime(key: any, value: any): any {
   // Server returns datetimes in the following format: '2017-05-09 18:30:00'
   if (typeof value === 'string' && /^\d{4}-\d\d-\d\d\ \d\d:\d\d:\d\d$/.test(value)) {
-    value = value.replace(' ', 'T');
-    return new Date(value);
+    const year = Number(value.substr(0, 4));
+    const month = Number(value.substr(5, 2)) - 1;
+    const date = Number(value.substr(8, 2));
+    const hours = Number(value.substr(11, 2));
+    const minutes = Number(value.substr(14, 2));
+    const seconds = Number(value.substr(17, 2));
+    return new Date(year, month, date, hours, minutes, seconds);
   }
 
   return value;
