d52c1ae6625938ff2eaafb5006ecdb56e03f665a
Job van Stiphout <job@manduro.nl>
2017-05-25 15:45:56 +0200

    feat(login): autofocus on input
    Closes #2

---
+++ src/directives/autofocus/autofocus.ts
@@ -0,0 +1,32 @@
+import { AfterViewInit, Directive, ElementRef, Renderer } from '@angular/core';
+import { Keyboard } from '@ionic-native/keyboard';
+import { Platform } from 'ionic-angular';
+
+@Directive({
+  selector: '[csrAutofocus]'
+})
+export class AutofocusDirective implements AfterViewInit {
+  constructor(
+    private renderer: Renderer,
+    private elementRef: ElementRef,
+    private keyboard: Keyboard,
+    private platform: Platform
+  ) {}
+
+  ngAfterViewInit() {
+    const element = this.elementRef.nativeElement.querySelector('input') as HTMLInputElement;
+    if (!element) {
+      return;
+    }
+
+    setTimeout(() => {
+      if (element.value && element.value.length > 0) {
+        return;
+      }
+      this.renderer.invokeElementMethod(element, 'focus', []);
+      if (this.platform.is('cordova')) {
+        this.keyboard.show();
+      }
+    }, 800);
+  }
+}
--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,7 +1,9 @@
+import { AutofocusDirective } from './autofocus/autofocus';
 import { MapsHrefDirective } from './maps-href/maps-href';
 import { SystemBrowserDirective } from './system-browser/system-browser';
 
 export const DIRECTIVES = [
+  AutofocusDirective,
   MapsHrefDirective,
   SystemBrowserDirective
 ];
